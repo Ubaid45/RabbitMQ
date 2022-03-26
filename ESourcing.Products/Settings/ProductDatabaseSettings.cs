eb463b267a440a663e16761999947378e75f655b
Ubaid Rana <ubaid.rana@appsfactory.de>
2022-03-26 19:59:22 +0100

    Added product microservice

---
+++ ESourcing.Products/Settings/ProductDatabaseSettings.cs
@@ -0,0 +1,8 @@
+namespace ESourcing.Products.Settings;
+
+public class ProductDatabaseSettings : IProductDatabaseSettings
+{
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
+    public string CollectionName { get; set; }
+}
